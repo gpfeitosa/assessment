7a234b65c03f22015e3e89ce2fcb54a2ebc569e0
Gustavo Feitosa <gpfeitosa@gmail.com>
2022-04-03 09:13:33 -0300

    Create createUserRequest

---
+++ api/src/main/java/com/kuehnenagel/assessment/vo/CreateUserRequest.java
@@ -0,0 +1,47 @@
+package com.kuehnenagel.assessment.vo;
+
+import javax.validation.constraints.NotBlank;
+
+public class CreateUserRequest {
+
+    @NotBlank
+    String name;
+
+    @NotBlank
+    String username;
+
+    @NotBlank
+    String password;
+
+    public CreateUserRequest() {}
+
+    public CreateUserRequest(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
