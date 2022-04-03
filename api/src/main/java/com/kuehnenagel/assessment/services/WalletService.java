6c2a03d35d3bab81936514f59e40a5698f9cbdb3
Gustavo Feitosa <gpfeitosa@gmail.com>
2022-04-03 14:01:59 -0300

    Create new wallet service methods

--- api/src/main/java/com/kuehnenagel/assessment/services/WalletService.java
+++ api/src/main/java/com/kuehnenagel/assessment/services/WalletService.java
@@ -4,7 +4,10 @@ import com.kuehnenagel.assessment.domain.User;
 import com.kuehnenagel.assessment.domain.Wallet;
 import com.kuehnenagel.assessment.repositories.WalletRepository;
 import com.kuehnenagel.assessment.utils.ContextUtils;
+import com.kuehnenagel.assessment.vo.DeleteWalletRequest;
 import com.kuehnenagel.assessment.vo.DepositRequest;
+import com.kuehnenagel.assessment.vo.TransferRequest;
+import com.kuehnenagel.assessment.vo.WithdrawalRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -43,5 +46,63 @@ public class WalletService {
         wallet.setBalance(newBalance);
 
         return walletRepository.update(wallet);
+    }
+
+    public Wallet makeWithdrawal(WithdrawalRequest withdrawalRequest) {
+        User user = contextUtils.getUserFromSecurityContext();
+
+        Wallet wallet = walletRepository.getByIdAndOwnerId(
+                withdrawalRequest.getWalletId(), user.getId()
+        );
+
+        if (wallet == null || wallet.getBalance() < withdrawalRequest.getAmount()) {
+            // TODO custom exception
+            return wallet;
+        }
+
+        Double newBalance = wallet.getBalance() - withdrawalRequest.getAmount();
+        wallet.setBalance(newBalance);
+
+        return walletRepository.update(wallet);
+    }
+
+    public void deleteWallet(DeleteWalletRequest deleteWalletRequest) {
+        User user = contextUtils.getUserFromSecurityContext();
+
+        Wallet wallet = walletRepository.getByIdAndOwnerId(
+                deleteWalletRequest.getWalletId(), user.getId()
+        );
+
+        if (wallet == null || wallet.getBalance() > 0) {
+            // TODO custom exception
+            return;
+        }
+
+        walletRepository.delete(wallet);
+    }
+
+    public void transfer(TransferRequest transferRequest) {
+        User user = contextUtils.getUserFromSecurityContext();
+
+        Wallet sourceWallet = walletRepository.getByIdAndOwnerId(
+                transferRequest.getSourceWalletId(), user.getId()
+        );
+
+        Double amount = transferRequest.getAmount();
+        if (sourceWallet == null || sourceWallet.getBalance() < amount) {
+            // TODO custom exceptions
+            return;
+        }
+
+        Wallet targetWallet = walletRepository.getById(transferRequest.getTargetWalletId());
+
+        Double newSourceBalance = sourceWallet.getBalance() - amount;
+        Double newTargetBalance = targetWallet.getBalance() + amount;
+
+        sourceWallet.setBalance(newSourceBalance);
+        targetWallet.setBalance(newTargetBalance);
+
+        walletRepository.update(sourceWallet);
+        walletRepository.update(targetWallet);
     }
 }
