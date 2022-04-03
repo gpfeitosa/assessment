3852ef6e229aeccf7c9229641ff8ac0e94a9df2a
Gustavo Feitosa <gpfeitosa@gmail.com>
2022-04-03 09:12:59 -0300

    Create walletRepository

---
+++ api/src/main/java/com/kuehnenagel/assessment/repositories/WalletRepository.java
@@ -0,0 +1,45 @@
+package com.kuehnenagel.assessment.repositories;
+
+import com.kuehnenagel.assessment.domain.User;
+import com.kuehnenagel.assessment.domain.Wallet;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class WalletRepository {
+
+    private final MongoOperations mongoOperations;
+
+    public WalletRepository(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public Wallet insert(Wallet wallet) {
+        return mongoOperations.insert(wallet);
+    }
+
+    public Wallet getById(String walletId) {
+        return mongoOperations.findById(walletId, Wallet.class);
+    }
+
+    public List<Wallet> getAllByUser(User user) {
+        Query query = Query.query(Criteria.where("owner.id").is(user.getId()));
+
+        return mongoOperations.find(query, Wallet.class);
+    }
+
+    public Wallet update(Wallet updatedWallet) {
+        Query query = Query.query(Criteria.where("_id").is(updatedWallet.getId()));
+
+        Update update = new Update();
+        update.set("balance", updatedWallet.getBalance());
+
+        mongoOperations.updateFirst(query, update, Wallet.class);
+        return updatedWallet;
+    }
+}
