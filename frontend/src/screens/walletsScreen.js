fbd04bce95d685f50c249c4f9216e7bbb115a27b
Gustavo Feitosa <gpfeitosa@gmail.com>
2022-04-07 23:31:26 -0300

    Finish wallets screen implementation

--- frontend/src/screens/walletsScreen.js
+++ frontend/src/screens/walletsScreen.js
@@ -1,31 +1,180 @@
 import { useEffect, useState } from 'react';
-import { Col, ListGroup, Row } from 'react-bootstrap';
+import {
+	Button,
+	Container,
+	ListGroup,
+	OverlayTrigger,
+	Row,
+	Tooltip,
+} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllWallets } from '../actions/walletActions';
+import {
+	deleteWallet,
+	getAllWallets,
+	makeDeposit,
+	makeWithdrawal,
+} from '../actions/walletActions';
+import CreateWalletForm from '../components/CreateWalletForm';
+import WalletItem from '../components/WalletItem';
+import {
+	IoArrowDownCircleOutline,
+	IoArrowUpCircleOutline,
+	IoTrashOutline,
+} from 'react-icons/io5';
+import AmountForm from '../components/AmountForm';
+import { logout } from '../utils/auth.utils';
+import Message from '../components/Message';
+import { useNavigate } from 'react-router-dom';
 
 const Wallets = () => {
-	const dispatch = useDispatch();
+	const [creatingNew, setCreatingNew] = useState(false);
+	const [makingDeposit, setMakingDeposit] = useState(false);
+	const [makingWithdraw, setMakingWithdraw] = useState(false);
 
-	const allWallets = useSelector((state) => state.allWallets);
-	console.log({ allWallets });
-	const { loading, error, wallets } = allWallets;
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const allWallets = useSelector((state) => state.walletReducer);
+	const { error, wallets } = allWallets;
 
 	useEffect(() => {
 		dispatch(getAllWallets());
 	}, [dispatch]);
 
+	const deleteWalletHandler = (id) => {
+		dispatch(deleteWallet(id));
+	};
+
+	const toggleMakingDeposit = () => {
+		setMakingDeposit(!makingDeposit);
+		setMakingWithdraw(false);
+	};
+
+	const toggleMakingWithdraw = () => {
+		setMakingWithdraw(!makingWithdraw);
+		setMakingDeposit(false);
+	};
+
+	const makeDepositHandler = (walletId, amount) => {
+		dispatch(makeDeposit(walletId, amount));
+		setMakingDeposit(false);
+	};
+
+	const makeWithdrawalHandler = (walletId, amount) => {
+		dispatch(makeWithdrawal(walletId, amount));
+		setMakingWithdraw(false);
+	};
+
+	const handleSignOut = () => {
+		logout();
+		navigate('/signin');
+	};
+
 	return (
-		<ListGroup variant="flush">
-			{wallets.map((wallet) => (
-				<ListGroup.Item key={wallet.id}>
-					<Row>
-						<Col md={2}>${wallet.id}</Col>
-						<Col md={2}>${wallet.owner.name}</Col>
-						<Col md={2}>${wallet.balance}</Col>
-					</Row>
-				</ListGroup.Item>
-			))}
-		</ListGroup>
+		<Container>
+			<Button
+				variant="outline-warning"
+				onClick={() => handleSignOut()}
+				style={{ margin: '8px' }}
+			>
+				Sign Out
+			</Button>
+			{!creatingNew && (
+				<Button
+					variant="outline-primary"
+					onClick={() => setCreatingNew(true)}
+					style={{ margin: '8px' }}
+				>
+					New Wallet
+				</Button>
+			)}
+			{error && <Message variant="danger">{error}</Message>}
+			{creatingNew && <CreateWalletForm setCreatingNew={setCreatingNew} />}
+			<ListGroup variant="flush" style={{ margin: '8px' }}>
+				<Row>
+					<WalletItem md={2}>Wallet Name</WalletItem>
+					<WalletItem md={2}>Owner</WalletItem>
+					<WalletItem md={2}>Balance</WalletItem>
+				</Row>
+				{wallets.map((wallet) => (
+					<ListGroup.Item key={wallet.id}>
+						<Row>
+							<WalletItem md={2}>{wallet.name}</WalletItem>
+							<WalletItem md={2}>{wallet.owner.name}</WalletItem>
+							<WalletItem md={2}>
+								$ {(Math.round(wallet.balance * 100) / 100).toFixed(2)}
+							</WalletItem>
+
+							{makingDeposit && !makingWithdraw && (
+								<WalletItem sm={2}>
+									<AmountForm
+										placeholder={'Deposit'}
+										walletId={wallet.id}
+										onSubmitHandler={makeDepositHandler}
+									/>
+								</WalletItem>
+							)}
+							<WalletItem sm={1}>
+								<OverlayTrigger
+									placement={'top'}
+									delay={{ show: 250, hide: 400 }}
+									overlay={<Tooltip id={'deposit-tooltip'}>Deposit</Tooltip>}
+								>
+									<Button
+										type="button"
+										variant="light"
+										onClick={() => toggleMakingDeposit()}
+									>
+										<IoArrowDownCircleOutline style={{ color: 'green' }} />
+									</Button>
+								</OverlayTrigger>
+							</WalletItem>
+
+							{!makingDeposit && makingWithdraw && (
+								<WalletItem sm={2}>
+									<AmountForm
+										placeholder={'Withdraw'}
+										walletId={wallet.id}
+										onSubmitHandler={makeWithdrawalHandler}
+									/>
+								</WalletItem>
+							)}
+							<WalletItem sm={1}>
+								<OverlayTrigger
+									placement={'top'}
+									delay={{ show: 250, hide: 400 }}
+									overlay={<Tooltip id={'withdraw-tooltip'}>Withdraw</Tooltip>}
+								>
+									<Button
+										type="button"
+										variant="light"
+										onClick={() => toggleMakingWithdraw()}
+									>
+										<IoArrowUpCircleOutline style={{ color: 'red' }} />
+									</Button>
+								</OverlayTrigger>
+							</WalletItem>
+
+							<WalletItem sm={1}>
+								<OverlayTrigger
+									placement={'top'}
+									delay={{ show: 250, hide: 400 }}
+									overlay={<Tooltip id={'delete-tooltip'}>Delete</Tooltip>}
+								>
+									<Button
+										type="button"
+										variant="light"
+										onClick={() => deleteWalletHandler(wallet.id)}
+									>
+										<IoTrashOutline style={{ color: 'red' }} />
+									</Button>
+								</OverlayTrigger>
+							</WalletItem>
+						</Row>
+					</ListGroup.Item>
+				))}
+			</ListGroup>
+		</Container>
 	);
 };
 
